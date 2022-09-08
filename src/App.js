150a778912a14dab19d7eca849aa5fe6313eda10
mikevuu <vuuthongnguyen@gmail.com>
2022-09-08 11:12:40 +0700

    chapter 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import logo from './logo.svg'
-
+import Header from './Header';
+import {useState, useEffect, useRef} from 'react';
 
 
 function App() {
+  const [inputValue,setInputValue] = useState("");
+  const prevInputValue = useRef("");
+
+  useEffect( () => {prevInputValue.current = inputValue;}, [inputValue]);
+
+  const changeValue = (e) => setInputValue(e.target.value);
+
   return (
     <div className="App">
-      Hello 1
-      <p> 
-        <img src ={logo}  alt = "logo"/>
-        <h2 className = "header__main"> this is to test</h2>
-        {/* press Shift + Alt + A to comment things out  */}
-      </p>
+       <input type = 'text' value = {inputValue} onChange = {changeValue} />
+       <h2> Current Value: {inputValue}</h2>
+       <h2> Previous Value : {prevInputValue.current}</h2>
     </div>
   );
 }
