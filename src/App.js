22c545691e7a218e3c31b74baea3742ba4f53c28
mikevuu <vuuthongnguyen@gmail.com>
2022-09-09 14:08:33 +0700

    chapter 3 - ctn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 import {useState, useEffect, useRef} from 'react';
 
 
 function App() {
-  const [inputValue,setInputValue] = useState("");
-  const prevInputValue = useRef("");
-
-  useEffect( () => {prevInputValue.current = inputValue;}, [inputValue]);
-
-  const changeValue = (e) => setInputValue(e.target.value);
 
   return (
     <div className="App">
-       <input type = 'text' value = {inputValue} onChange = {changeValue} />
-       <h2> Current Value: {inputValue}</h2>
-       <h2> Previous Value : {prevInputValue.current}</h2>
+      <Header/>
+      <Content/>
+
     </div>
   );
 }
