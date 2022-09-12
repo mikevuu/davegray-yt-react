92551f5793f56307681758b1113e4ecb94d05b98
mikevuu <vuuthongnguyen@gmail.com>
2022-09-12 18:52:45 +0700

    chapter7

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import dataItem from './data/dataItem';
+import { useState } from 'react';
 function Content() {
-  const handleClick = () => {
-    console.log('You clikced it');
-  };
-
+  const [items, setItems] = useState(dataItem);
   return (
-    <div>
-      Content
-      <button onClick={handleClick}> Click it </button>
-    </div>
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input type='checkbox' checked={item.checked} />
+            <label> {item.item}</label>
+            <button> Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
