2b125cbdb59b9a0eac9ac990a9d6c541df3d02f1
mikevuu <vuuthongnguyen@gmail.com>
2022-09-12 20:55:59 +0700

    latest

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import dataItem from './data/dataItem';
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 function Content() {
   const [items, setItems] = useState(dataItem);
+  const handleCheck = (id) => {
+    const dataItemChange = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(dataItemChange);
+    localStorage.setItem('shoppingList', JSON.stringify(dataItemChange));
+  };
   return (
     <main>
       <ul>
         {items.map((item) => (
           <li className='item' key={item.id}>
-            <input type='checkbox' checked={item.checked} />
+            <input
+              type='checkbox'
+              checked={item.checked}
+              onChange={() => handleCheck(item.id)}
+            />
             <label> {item.item}</label>
-            <button> Delete</button>
+            <FaTrashAlt role='button' tabIndex='0' />
           </li>
         ))}
       </ul>
